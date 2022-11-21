55e2feb4465b50ad9858e88bc53e8a9fbf8803ef
AsHansen88 <Ande970s@stud.kea.dk>
2022-11-21 13:20:19 +0100

    exam Start

---
+++ src/main/java/com/example/bilabonomenteksam/BilAbonomentEksamApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilabonomenteksam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilAbonomentEksamApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BilAbonomentEksamApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/bilabonomenteksam/BilAbonomentEksamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilabonomenteksam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilAbonomentEksamApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
